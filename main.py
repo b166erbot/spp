a47c6c6dd33b7b9f671dd339faada8a8f696c05f
b166erobot <bernard0freitas@outlook.com>
2020-01-27 13:03:56 -0300

    pequeno update para rodar de qualquer lugar/pasta.

--- main.py
+++ main.py
@@ -1,5 +1,9 @@
 #!/usr/bin/python3
 from spp.spp import main
+from os import chdir
+from pathlib import Path
 
 if __name__ == '__main__':
+    local_deste_arquivo = Path(__file__).parent
+    chdir(local_deste_arquivo)
     main()
